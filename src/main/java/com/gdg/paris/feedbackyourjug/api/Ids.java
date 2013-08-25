d58b2ff7c6412d09e278b496198e9549f50c8607
Thibault Pouget <thibault.pouget@cirruseo.com>
2013-08-25 21:41:14 +0200

    set actual client id

--- src/main/java/com/gdg/paris/feedbackyourjug/api/Ids.java
+++ src/main/java/com/gdg/paris/feedbackyourjug/api/Ids.java
@@ -4,7 +4,7 @@ package com.gdg.paris.feedbackyourjug.api;
  * Contains the client IDs for allowed clients consuming the helloworld API.
  */
 public class Ids {
-  public static final String WEB_CLIENT_ID = "replace this with your web client application ID";
+  public static final String WEB_CLIENT_ID = "700903555117.apps.googleusercontent.com";
   public static final String ANDROID_CLIENT_ID = "replace this with your Android client ID";
   public static final String IOS_CLIENT_ID = "replace this with your iOS client ID";
   public static final String ANDROID_AUDIENCE = WEB_CLIENT_ID;
