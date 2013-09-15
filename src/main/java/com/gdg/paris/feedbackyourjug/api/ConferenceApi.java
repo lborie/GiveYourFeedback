6d81a30309efbaa68a3b56c359cfce3f608e7251
Ludovic Borie <l.borie@kanithael.net>
2013-09-15 03:21:08 +0200

    Fonctionnalités supplémentaires : Mail, 1.8.4, validation

--- src/main/java/com/gdg/paris/feedbackyourjug/api/ConferenceApi.java
+++ src/main/java/com/gdg/paris/feedbackyourjug/api/ConferenceApi.java
@@ -7,14 +7,30 @@ import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.appengine.api.oauth.OAuthRequestException;
 import com.google.appengine.api.users.User;
+import com.google.common.base.Predicate;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
 import com.google.gdata.client.spreadsheet.SpreadsheetService;
-import com.google.gdata.data.spreadsheet.*;
+import com.google.gdata.data.spreadsheet.ListEntry;
+import com.google.gdata.data.spreadsheet.ListFeed;
+import com.google.gdata.data.spreadsheet.SpreadsheetEntry;
+import com.google.gdata.data.spreadsheet.WorksheetEntry;
 import com.google.gdata.util.ServiceException;
+import com.google.gdata.util.common.base.Nullable;
 
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * API to manage the conferences.
@@ -22,13 +38,16 @@ import java.util.List;
  */
 @Api(
         name = "feedbackyourjug",
-        version = "v1",
+        version = "v2",
         clientIds = {Ids.WEB_CLIENT_ID, Ids.API_EXPLORER_ID}
 )
 public class ConferenceApi {
 
     private final GenericDao<Conference> conferenceDao = new GenericDao<>(Conference.class);
     private final GenericDao<Session> sessionDao = new GenericDao<>(Session.class);
+    private final static Logger logger = Logger.getLogger(ConferenceApi.class.getName());
+
+    private final static String ADMINISRATOR = "l.borie@gmail.com";
 
     @ApiMethod(
             name = "conferences.list",
@@ -36,7 +55,15 @@ public class ConferenceApi {
             httpMethod = ApiMethod.HttpMethod.GET
     )
     public List<Conference> getConferences() {
-        return conferenceDao.getEntities();
+        List<Conference> allConferences = conferenceDao.getEntities();
+        Predicate<Conference> validatedConference = new Predicate<Conference>() {
+            @Override
+            public boolean apply(@Nullable Conference conference) {
+                return Boolean.TRUE.equals(conference.isValidated());
+            }
+        };
+        return Lists.newArrayList(Iterables.filter(allConferences, validatedConference));
+//        return conferenceDao.getEntities();
     }
 
     @ApiMethod(
@@ -45,11 +72,17 @@ public class ConferenceApi {
             httpMethod = ApiMethod.HttpMethod.POST
     )
     public Conference insertConference(Conference conference, User user) throws OAuthRequestException,
-            IOException, ServiceException {
+            IOException, ServiceException, MessagingException {
         if (user == null) {
             throw new OAuthRequestException("Invalid user.");
         }
 
+        // Owner
+        conference.setOwnerUserEmail(user.getEmail());
+        conference.setValidated(false);
+        conferenceDao.insertEntity(conference);
+
+        // Sessions
         SpreadsheetService service = new SpreadsheetService("give-your-feedback");
         service.setAuthSubToken(conference.getUserToken());
         service.useSsl();
@@ -57,8 +90,6 @@ public class ConferenceApi {
         URL spreadsheetUrl = new URL("https://spreadsheets.google.com/feeds/spreadsheets/" + conference.getSpreadSheetId());
         SpreadsheetEntry entry = service.getEntry(spreadsheetUrl, SpreadsheetEntry.class);
         WorksheetEntry wsEntry = entry.getDefaultWorksheet();
-
-        conferenceDao.insertEntity(conference);
 
         List<Session> sessions = new ArrayList<>();
 
@@ -68,7 +99,7 @@ public class ConferenceApi {
             session.setIdConference(conference.getId());
             sessions.add(session);
             for (String tag : row.getCustomElements().getTags()) {
-                switch (tag){
+                switch (tag) {
                     case "titre":
                         session.setTitle(row.getCustomElements().getValue(tag));
                         break;
@@ -99,16 +130,30 @@ public class ConferenceApi {
         }
         sessionDao.insertEntities(sessions);
 
+        sendMail(conference);
+
         return conference;
     }
 
-    @ApiMethod(
-            name = "conferences.update",
-            path = "conference",
-            httpMethod = ApiMethod.HttpMethod.PUT
-    )
-    public Conference updateConference(Conference conference) {
-        conferenceDao.insertEntity(conference);
-        return conference;
+    private void sendMail(Conference conference) throws UnsupportedEncodingException, MessagingException {
+        Properties props = new Properties();
+        javax.mail.Session session = javax.mail.Session.getDefaultInstance(props, null);
+
+        try {
+            String encodingOptions = "text/html; charset=UTF-8";
+            MimeMessage msg = new MimeMessage(session);
+            msg.setHeader("Content-Type", encodingOptions);
+            msg.setFrom(new javax.mail.internet.InternetAddress(ADMINISRATOR, "Give Your Feedback"));
+            msg.addRecipient(Message.RecipientType.TO,
+                    new InternetAddress(ADMINISRATOR));
+            msg.setSubject("Nouvelle conférence créée !", "UTF-8");
+            msg.setText("Une nouvelle conférence vient d'être créée (" + conference.getName() + "), et est en attente de votre validation.");
+
+            Transport.send(msg);
+
+        } catch (UnsupportedEncodingException | MessagingException e) {
+            logger.log(Level.SEVERE, "Mail send error", e);
+            throw e;
+        }
     }
 }
